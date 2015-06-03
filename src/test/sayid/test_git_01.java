a1793d67c44141dae318121e222077f61e9aeedf
Sayid <kttao7758258@163.com>
2015-06-02 18:52:10 -0700

    Fouth commot

--- src/test/sayid/test_git_01.java
+++ src/test/sayid/test_git_01.java
@@ -5,6 +5,7 @@ public class test_git_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Sayid, testing GIT");
+		System.out.println("Hi how are you!");
 	}
 
 }
